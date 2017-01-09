46dbefed63963dea72c864b2cdfe40ef4e52a8c7
Ben Howell <bjhowell@gmail.com>
2017-01-10 05:51:34 +1100

    added display-area

--- quill.cljs
+++ quill.cljs
@@ -96,3 +96,26 @@
          [:div {:id (str "quill-editor-" id)
                 :class "quill-editor"
                 :dangerouslySetInnerHTML {:__html content}}]])})))
+
+  
+(defn display-area [{:keys [id content]}]
+  (let [this (r/atom nil)]
+    (r/create-class
+     {:component-did-mount
+      (fn [component]
+        (reset! this (js/Quill. (r/dom-node component)
+                                #js {:theme "snow"
+                                     :placeholder "Compose an epic..."}))
+        (.disable @this))
+
+      :component-will-receive-props
+      (fn [component next-props]
+        (.pasteHTML @this (:content (second next-props))))
+
+      :display-name  (str "quill-display-area-" id)
+
+      :reagent-render
+      (fn []
+        [:div {:id (str "quill-display-area-" id)
+               :class "quill-display-area"
+               :dangerouslySetInnerHTML {:__html content}}])})))
